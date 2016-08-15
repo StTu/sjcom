4b6bcd91bc41e76d55a8f131df1650f5ffba2b27
Stuart James <stuart@stuart-james.net>
2016-08-15 16:57:19 +0100

    date

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+jekyll build --destination /vol/websites/sjcom
+cpwd=pwd
+cd /vol/websites/sjcom
+cd Publications/
+mogrify -resize 200x -format png */*.png
+mogrify -resize 200x -format png */*.jpg
+cd ..
+git add *
+val=date
+git commit -m "$val"
+git push
+cd pwd
