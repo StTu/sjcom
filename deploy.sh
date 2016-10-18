c90f6ff93c860eb14d3fd1303a13ca62a99cec51
Stuart James <stuart@stuart-james.net>
2016-10-18 22:50:27 +0100

    date

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,14 @@ cpwd=pwd
 cd /vol/websites/sjcom
 cd publications/
 mogrify -resize 200x -format png */*.png
-mogrify -resize 200x -format png */*.jpg
+mogrify -resize 200x -format jpg */*.jpg
+cd ..
+cd images/
+mogrify -resize '400x>' -format png */*.png
+mogrify -resize '400x>' -format jpg */*.jpg
 cd ..
 git add *
 val=date
 git commit -m "$val"
-git push
+git push --force
 cd pwd
